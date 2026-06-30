c4a6353aa29d8b4ab7971ea8721b7496d4daf6a7
Nianyu Su <mirakelor@outlook.com>
2026-06-30 22:58:30 +0800

    feat: Add loading animation with cat loader and styles for improved user experience

--- BlackboardLM/components/layout.py
+++ BlackboardLM/components/layout.py
@@ -12,6 +12,26 @@ from BlackboardLM.components.input_bar import input_bar
 def index() -> rx.Component:
     return rx.fragment(
         global_styles(),
+        rx.html("""
+        <div id="loading">
+            <div class="cat">
+                <div class="body"></div>
+                <div class="head">
+                    <div class="face"></div>
+                </div>
+                <div class="foot">
+                    <div class="tummy-end"></div>
+                    <div class="bottom"></div>
+                    <div class="legs left"></div>
+                    <div class="legs right"></div>
+                </div>
+                <div class="paw">
+                    <div class="hands left"></div>
+                    <div class="hands right"></div>
+                </div>
+            </div>
+        </div>
+        """),
         rx.box(
             rx.cond(
                 AppState.theme_name == _theme.THEME_HOGWARTS,
